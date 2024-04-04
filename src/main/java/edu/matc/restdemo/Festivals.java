f08c65d40a0f1bb7b8233d6b20a4c32ca7f66668
jbjohnson2 <jbjohnso2@madisoncollege.edu>
2024-04-04 17:27:19 -0500

    added methods that return JSON in festivals

--- src/main/java/edu/matc/restdemo/Festivals.java
+++ src/main/java/edu/matc/restdemo/Festivals.java
@@ -5,7 +5,6 @@ import edu.matc.entity.Region;
 import edu.matc.entity.Type;
 import edu.matc.persistence.GenericDao;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
@@ -25,6 +24,10 @@ public class Festivals {
     private final Logger logger = LogManager.getLogger(this.getClass());
     private String output;
 
+    ObjectMapper objectMapper;
+
+
+
     /**
      * Instantiates a new Festival.
      */
@@ -33,108 +36,14 @@ public class Festivals {
         genericDao = new GenericDao<>(Festival.class);
     }
 
-    @GET
-    // The Java method will produce content identified by the MIME Media type "text/plain"
-    @Produces("text/plain")
-
-    /**
-     * This method's purpose is to return all festivals
-     */
-    public Response getFestivals() {
-        // Return a simple message
-        output = "Here's all the festivals: " + genericDao.getAll();
-        return Response.status(200).entity(output).build();
-    }
 
     @GET
 
-    @Path("/{param}")
-    
-    @Produces("text/plain")
-
-    /**
-     * This method's purpose is to get the festival by id
-     */
-    public Response getFestivalById(@PathParam("param") int id) {
-
-        // Return the festival specified by the id
-        if (genericDao.getById(id) == null) {
-
-            output = "No festivals could be found";
-
-        } else {
-
-            output = "Here's the festival: " + genericDao.getById(id);
-        }
-
-        return Response.status(200).entity(output).build();
-    }
-
-    /**
-     * Gets festival by type id.
-     *
-     * @param typeID the id
-     * @return the festival by type id
-     */
-    @GET
-
-    @Path("/types/{param}")
-
-    @Produces("text/plain")
-
-    public Response getFestivalByTypeId(@PathParam("param") int typeID) {
-
-        // Return the festival specified by the id
-        if (genericDao.findByPropertyEqual("typeID", typeID) == null) {
-
-            output = "No festivals could be found";
-
-        } else {
-
-            output = "Here's the festival: " + genericDao.findByPropertyEqual("typeID", typeID);
-        }
-
-        return Response.status(200).entity(output).build();
-    }
-
-
-    /**
-     * Gets festival by region id.
-     *
-     * @param regionID the region id
-     * @return the festival by region id
-     */
-    @GET
-
-    @Path("/regions/{param}")
-
-    @Produces("text/plain")
-
-    public Response getFestivalByRegionId(@PathParam("param") int regionID) {
-
-        // Return the festival specified by the id
-        if (genericDao.findByPropertyEqual("regionID", regionID) == null) {
-
-            output = "No festivals could be found";
-
-        } else {
-
-            output = "Here's the festival: " + genericDao.findByPropertyEqual("regionID", regionID);
-        }
-
-        return Response.status(200).entity(output).build();
-    }
-
-    // The Java method will process HTTP GET requests
-    @GET
-
-    @Path("/json")
-    // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("application/json")
-    public Response getMessage() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        GenericDao<Festival> festivalDao = new GenericDao<>(Festival.class);
-        List<Festival> festivals = festivalDao.getAll();
+    public Response getFestivals() {
+        objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        List<Festival> festivals = genericDao.getAll();
         String festivalsJsonString = "Error making json file";
 
         try {
@@ -149,13 +58,196 @@ public class Festivals {
         return Response.status(500).entity(festivalsJsonString).build();
     }
 
+
+    /**
+     * Gets all festivals in plain text.
+     *
+     * @return the festivals in plain text
+     */
+    @GET
+    @Path("/pt")
+    @Produces("text/plain")
+
+
+    public Response getFestivalsPT() {
+        // Return a simple message
+        output = "Here's all the festivals: " + genericDao.getAll();
+        return Response.status(200).entity(output).build();
+    }
+
+    /**
+     * get festival by id
+     * @param id festival to be retrieved id
+     * @return festival
+     */
+
+    @GET
+    @Path("/{param}")
+    @Produces("application/json")
+    public Response getFestivalById(@PathParam("param") int id) {
+        objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+
+        Festival festival = (Festival)genericDao.getById(id);
+
+        String festivalString = "Error making json file";
+
+        try {
+            festivalString = objectMapper.writeValueAsString(festival);
+
+            logger.info(festivalString);
+            logger.info(Response.status(200).entity(festivalString).build());
+            return Response.status(200).entity(festivalString).build();
+
+        } catch (JsonProcessingException jsonProcessingException) {
+            logger.error("Error making json file", jsonProcessingException);
+        }
+
+        return Response.status(500).entity(festivalString).build();
+    }
+
+    /**
+     * This method's purpose is to get the festival by id
+     */
+
+    @GET
+
+    @Path("/{param}/pt")
+    
+    @Produces("text/plain")
+
+
+    public Response getFestivalByIdPT(@PathParam("param") int id) {
+
+        // Return the festival specified by the id
+        if (genericDao.getById(id) == null) {
+
+            output = "No festivals could be found";
+
+        } else {
+
+            output = "Here's the festival: " + genericDao.getById(id);
+        }
+
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/types/{param}")
+    @Produces("application/json")
+    public Response getFestivalsByTypeId(@PathParam("param") int typeId) {
+        objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+
+
+        List festivals = genericDao.findByPropertyEqual("typeID", typeId);
+
+        String festivalString = "Error making json file";
+
+        try {
+            festivalString = objectMapper.writeValueAsString(festivals);
+
+            logger.info(festivalString);
+            logger.info(Response.status(200).entity(festivalString).build());
+            return Response.status(200).entity(festivalString).build();
+
+        } catch (JsonProcessingException jsonProcessingException) {
+            logger.error("Error making json file", jsonProcessingException);
+        }
+
+        return Response.status(500).entity(festivalString).build();
+    }
+
+    /**
+     * Gets festival by type id.
+     *
+     * @param typeID the type id
+     * @return the festival by type id
+     */
+    @GET
+
+    @Path("/types/{param}/pt")
+
+    @Produces("text/plain")
+
+    public Response getFestivalByTypeIdPT(@PathParam("param") int typeID) {
+
+        // Return the festival specified by the id
+        if (genericDao.findByPropertyEqual("typeID", typeID) == null) {
+
+            output = "No festivals could be found";
+
+        } else {
+
+            output = "Here's the festival: " + genericDao.findByPropertyEqual("typeID", typeID);
+        }
+
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/regions/{param}")
+    @Produces("application/json")
+    public Response getFestivalsByRegionId(@PathParam("param") int regionId) {
+        objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+
+
+        List festivals = genericDao.findByPropertyEqual("regionID", regionId);
+
+        String festivalString = "Error making json file";
+
+        try {
+            festivalString = objectMapper.writeValueAsString(festivals);
+
+            logger.info(festivalString);
+            logger.info(Response.status(200).entity(festivalString).build());
+            return Response.status(200).entity(festivalString).build();
+
+        } catch (JsonProcessingException jsonProcessingException) {
+            logger.error("Error making json file", jsonProcessingException);
+        }
+
+        return Response.status(500).entity(festivalString).build();
+    }
+
+    /**
+     * Gets festival by region id.
+     *
+     * @param regionID the region id
+     * @return the festival by region id
+     */
+    @GET
+
+    @Path("/regions/{param}/pt")
+
+    @Produces("text/plain")
+
+    public Response getFestivalByRegionIdPT(@PathParam("param") int regionID) {
+
+        // Return the festival specified by the id
+        if (genericDao.findByPropertyEqual("regionID", regionID) == null) {
+
+            output = "No festivals could be found";
+
+        } else {
+
+            output = "Here's the festival: " + genericDao.findByPropertyEqual("regionID", regionID);
+        }
+
+        return Response.status(200).entity(output).build();
+    }
+
+
+
+    /**
+     * This method's purpose is to add a  festival
+     */
     @POST
 
     @Path("/{name}/{regionId}/{typeId}/{date}")
 
-    /**
-     * This method's purpose is to return all festivals
-     */
+
     public Response postFestivals(@PathParam("name") String name,
                                   @PathParam("regionId") int regionId,
                                   @PathParam("typeId") int typeId,
